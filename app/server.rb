a9189cd6addffb207dbd76d5f147aced3ee8c8ae
Nick Dyer <nbdyer@gmail.com>
2014-11-22 18:25:56 +0000

    Fixed persistent flash notice

--- app/server.rb
+++ app/server.rb
@@ -67,7 +67,7 @@ class Chitter < Sinatra::Base
       session[:user_id] = user.id
       redirect to('/')
     else
-      flash[:errors] = ["The email or password is incorrect"]
+      flash.now[:errors] = ["The email or password is incorrect"]
       erb :"sessions/new"
     end
   end
